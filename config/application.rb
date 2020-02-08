32a09d92f662f6d17cbdb408e19d4029b25038f4
wangxuefei <xuefeiwang@jinshuju.net>
2020-02-08 17:49:55 +0800

    enable request for graphql in the app

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,12 @@ module Reminder
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origin '*'
+        resource '/graphql', headers: :any, methods: %i[post options]
+      end
+    end
   end
 end
